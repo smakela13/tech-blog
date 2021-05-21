3a1f53399e8f57abbdc683a221050a84bab4135a
SMakela <73409799+smakela13@users.noreply.github.com>
2021-05-20 20:34:59 -0400

    Finished adding Post.js and Comment.js models

---
+++ models/Comment.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment_body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
