1f4381f195f001df2b9bc17e6b2969ba11e03d53
SMakela <73409799+smakela13@users.noreply.github.com>
2021-06-04 16:01:19 -0400

    Updated README file - Added automatic logout functionality - Added screenshots and favicons - Added favicons to dashboard and main.handlebars

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ const sequelize = require('./config/config');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: "Super secret secret",
+  cookie: { maxAge: 30000 }, // 30 seconds idle time
+  rolling: true, // Resets cookie maxAge with each request
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
