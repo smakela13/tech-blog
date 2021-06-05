b6534cb093f046b77a171942b5e6a5ca068aa19b
SMakela <73409799+smakela13@users.noreply.github.com>
2021-06-04 23:10:01 -0400

    Added an alert to show a user has successfully logged out

--- public/js/logout.js
+++ public/js/logout.js
@@ -6,7 +6,7 @@ const logout = async function() {
 
   if (response.ok) {
     document.location.replace('/');
-    alert(`Log out successful`)
+    alert(`Log out successful`);
   } else {
     alert('Failed to log out');
   }
