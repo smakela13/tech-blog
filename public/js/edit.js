805e79890774d876ab0ff3c9d838a6ed2cd48b88
SMakela <73409799+smakela13@users.noreply.github.com>
2021-06-09 21:37:59 -0400

    Fixed an issue where the post wasn't updating.

--- public/js/edit.js
+++ public/js/edit.js
@@ -9,8 +9,8 @@ const editFormHandler = async function(event) {
   await fetch(`/api/post/${postId}`, {
     method: 'PUT',
     body: JSON.stringify({
-      title,
-      body
+      post_title: title,
+      post_body: body
     }),
     headers: {
       'Content-Type': 'application/json'
